cb5b5692ad2e34b0df51945893ef3ba6ce19d1bf
HanaaSous <81621266+HanaaSous@users.noreply.github.com>
2021-07-01 16:25:40 +0300

    Create input.sh

---
+++ input.sh
@@ -0,0 +1,6 @@
+#paths for filer repos and local repos
+
+nutanix_cvm_3rdparty_filer_location=/home/hanna.sous/users/nutanix-cvm-3rdparty
+nutanix_cvm_3rdparty_local_location=/home/hanna.sous/filer.dev.eng.nutanix.com:8080/Users/nutanix-cvm-3rdparty
+nutanix_cvm_test_filer_location=/home/hanna.sous/users/nutanix-cvm-test
+nutanix_cvm_test_local_location=/home/hanna.sous/filer.dev.eng.nutanix.com:8080/Users/nutanix-cvm-test
